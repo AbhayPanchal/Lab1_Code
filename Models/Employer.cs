54a69b40bd6c7856fc46862652cb54ffae35db72
Abhay Panchal <abhaypanchal32@gmail.com>
2024-10-06 14:27:27 -0400

    Initial commit with Lab1 code and .gitignore

---
+++ Models/Employer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class Employer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Website { get; set; }
+
+        [Display(Name = "Incorporated Date")]
+        [DataType(DataType.Date)]
+        public DateTime? IncorporatedDate { get; set; }
+    }
+}
